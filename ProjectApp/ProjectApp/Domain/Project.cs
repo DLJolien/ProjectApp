ffd0caa423c4507168077511a86248550ad83754
Jolien De Letter <joliendeletter@gmail.com>
2020-06-22 10:02:51 +0200

    added Project, ProjectTag, Status and Tag classes

---
+++ ProjectApp/ProjectApp/Domain/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectApp.Domain
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<ProjectTag> ProjectTags { get; set; }
+        public int StatusId { get; set; }
+        public Status Status { get; set; }
+    }
+}
