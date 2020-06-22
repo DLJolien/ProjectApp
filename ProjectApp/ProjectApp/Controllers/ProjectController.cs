848e50726b7a2e6ad5e3687900a42fe65d222c3f
Jolien De Letter <joliendeletter@gmail.com>
2020-06-22 14:21:23 +0200

    added Detail view

--- ProjectApp/ProjectApp/Controllers/ProjectController.cs
+++ ProjectApp/ProjectApp/Controllers/ProjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,10 +20,10 @@ namespace ProjectApp.Controllers
             _projectDb = projectDb;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             List<ProjectListViewModel> vmList = new List<ProjectListViewModel>();
-            var projects = _projectDb.Projects.Include(x => x.Status).Include(x => x.ProjectTags).ThenInclude(x => x.Tag);
+            var projects = await _projectDb.Projects.Include(x => x.Status).Include(x => x.ProjectTags).ThenInclude(x => x.Tag).ToListAsync();
             foreach (var project in projects)
             {
                 ProjectListViewModel vm = new ProjectListViewModel()
@@ -39,5 +40,20 @@ namespace ProjectApp.Controllers
 
             return View(vmList);
         }
+        public async Task<IActionResult> Detail(int id)
+        {
+            Project project = await _projectDb.Projects.Include(x => x.Status).Include(x => x.ProjectTags).ThenInclude(x => x.Tag).FirstOrDefaultAsync(x => x.Id == id);
+            ProjectDetailViewModel vm = new ProjectDetailViewModel()
+            {
+                Title = project.Title,
+                CompletionDate = project.CompletionDate,
+                Status = project.Status.Description,
+                Tags = project.ProjectTags.Select(x => x.Tag.Description).ToList(),
+                PhotoUrl = project.PhotoUrl,
+                Description = project.Description
+            };
+            return View(vm);
+        }
+
     }
 }
